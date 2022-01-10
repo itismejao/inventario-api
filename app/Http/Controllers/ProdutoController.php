1e606cf30b135997da3e5016e565017e5897e951
Gabriel Biziach <gabriel.biziach@novomundo.com.br>
2022-01-10 09:31:59 -0300

    .

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -17,7 +17,7 @@ class ProdutoController extends Controller
 
     public function getProductPagination($init, $end) {
         $result = DB::select("select * from (select m.*, rownum r from vw_produto_ids m)
-            where r >= {$init} and r < {$end}");
+            where r >= {$init} and r <= {$end}");
     
         return response()->json($result, 200);
     }
