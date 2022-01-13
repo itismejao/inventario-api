3573f842dfc6711072c7ba86c05b8dc9b04615fc
1033295 <mateus.luiz@novomundo.com.br>
2022-01-13 09:49:08 -0300

    correção api

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -16,12 +16,12 @@ class ProdutoController extends Controller
     }
 
     public function getProductPagination($init, $end) {
-        $result = DB::select("select tabela2.id_produto, tabela2.ean, tabela2.nome
-        from (select tabela.*, rownum r
-        from (select m.* from vw_produto_ids m order by m.id_produto asc) tabela
-        order by tabela.id_produto asc) tabela2
-        where tabela2.r >= {$init} and tabela2.r <= {$end}
-        order by tabela2.id_produto desc");
+        $result = DB::select("select tabela2.id_produto, tabela2.ean, tabela2.nome, tabela2.r
+                            from (select tabela.*, rownum r
+                                from (select m.* from vw_produto_ids m order by m.id_produto desc) tabela
+                                order by tabela.id_produto desc) tabela2
+                                where tabela2.r >= {$init} and tabela2.r <= {$end}
+                                order by tabela2.id_produto desc");
     
         return response()->json($result, 200);
     }
