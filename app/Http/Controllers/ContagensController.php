18a493b363f6d78366a98de508bf2fb16ea86c0f
1033295 <mateus.luiz@novomundo.com.br>
2022-01-24 17:20:38 -0300

    correção

--- app/Http/Controllers/ContagensController.php
+++ app/Http/Controllers/ContagensController.php
@@ -21,7 +21,7 @@ class ContagensController extends Controller
             foreach($data as $key){
                 DB::insert('insert into inventario_cic_imp_afrot 
                     (id_filial, id_funcionario, id_agrupador, data_cria_agrupador, data_inicia_agrupador, data_termino_agrupador, id_item_inventario, id_produto, ean, id_unico, quantidade, status, aplicacao, local) 
-                    values (?,?,?,sysdate,sysdate,sysdate,?,?,?,?,?,?,?)', 
+                    values (?,?,?,sysdate,sysdate,sysdate,?,?,?,?,?,?,?,?)', 
                             [$id_filial,$id_funcionario,$id_agrupador,null,$key['id_produto'],$key['ean'],null,$key['quantidade'],1,2,$key['tipo_local']]);
             }
     
